5e9c72f976c2da9bf55673d510ab892cf3eee4ed
Rui Machado <rm.rodrigues.machado@gmail.com>
2023-05-26 12:10:08 -0400

    week3_models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
 # from .models import related models
+from djangoapp.models import CarMake
+from djangoapp.models import CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline (admin.StackedInline):
+    model = CarModel
+    extra = 5
 
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ['Type', 'Dealear_id', 'Name', 'Description']
 # CarModelAdmin class
+admin.site.register(CarModel)
 
 # CarMakeAdmin class with CarModelInline
-
-# Register models here
+class CarMakeAdmin(admin.ModelAdmin):
+    fields = ['Name', 'Description']
+    inlines = [CarModelInline]
+admin.site.register(CarMake)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -10,6 +10,17 @@ from django.utils.timezone import now
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
+class CarMake(models.Model):
+
+    name = models.CharField(null=False, max_length=30, default='ford')
+    description = models.CharField(null=False, max_length=30, default='none')
+    
+    # Create a toString method for object string representation
+
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+                "Description: " + self.description
+
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -19,6 +30,35 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+
+class CarModel(models.Model):
+
+    SEDAN = 'sedan'
+    SUV = 'suv'
+    WAGON = 'wagon'
+    TYPES_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+    ]
+    CMake = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    Name = models.CharField(null=False, max_length=30, default='ford')
+    Dealer_id = models.IntegerField()
+    Description = models.CharField(null=False, max_length=30, default='none')
+    Type = models.CharField(
+        null=False,
+        max_length=20,
+        choices=TYPES_CHOICES,
+        default=SEDAN
+    )
+    Year = models.DateField(null=True)
+   
+    # Create a toString method for object string representation
+    def __str__(self):
+         return "Name: " + self.Name + "," + \
+                "Description: " + self.Description + ", " + \
+                "Type: " + self.Type + ", " + \
+                "Year: " + str(self.Year)
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
